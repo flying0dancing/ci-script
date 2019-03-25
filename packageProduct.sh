82e62e65b20c57b6447253544b5b67ebdd9cba67
Kun Shen <Kun.Shen@lombardrisk.com>
2019-03-25 17:03:40 +0800

    useful script

--- packageProduct.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-selfPath=`dirname $(readlink -f "$0")`
-sourchFullPath=${selfPath}/../$1
-zipFullPath=${selfPath}/../$1
-properties=${selfPath}/../$2
-echo ${sourchFullPath} ${zipFullPath} ${properties} $3
-sh ${selfPath}/zipProduct.sh ${sourchFullPath} ${zipFullPath} ${properties} $3

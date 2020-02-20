4979e546f3dda4f1dd0bc74860d48248eacc4d0f
Kun Shen <kshen@vermeg.com>
2020-02-20 17:30:26 +0800

    fix search installer at local server issue 9

--- vars/copyScripts.groovy
+++ vars/copyScripts.groovy
@@ -6,6 +6,6 @@
 def call(propertiesSet){
     def app_hostuser=propertiesSet['app.user']+'@'+propertiesSet['app.host']
     def downloadPath='/home/'+propertiesSet['app.user']+'/'
-    sh( returnStatus: true, script: '''scp scripts/Remote*.sh '''+app_hostuser+''':'''+downloadPath)
+    sh( returnStatus: true, script: '''scp '''+env.WORKSPACE+'''/scripts/Remote*.sh '''+app_hostuser+''':'''+downloadPath)
     sh(returnStdout: true, script: '''ssh '''+app_hostuser+''' 'chmod u+x Remote*.sh ' ''')
 }

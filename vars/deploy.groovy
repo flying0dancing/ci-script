49fec5aad01686bdfd4f684b844764a2d4d30cde
Kun Shen <kshen@vermeg.com>
2020-03-12 17:49:57 +0800

    add function for new envVars 2

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,4 +1,4 @@
-def call(String jobName, String projectFolder, String repoName, String repoBranch){
+def triggerOtherJob(String jobName, String projectFolder, String repoName, String repoBranch){
     def branches=[:]
     def MAX_CONCURRENT = 2
     //create a fifo
@@ -33,3 +33,80 @@ def call(String jobName, String projectFolder, String repoName, String repoBranc
     }
     parallel branches
 }
+
+
+def inOneJob(String projectFolder){
+    def branches=[:]
+    def MAX_CONCURRENT = 2
+    //create a fifo
+    latch = new java.util.concurrent.LinkedBlockingDeque(MAX_CONCURRENT)
+    //put resource in fifo
+    for(int i=0; i<MAX_CONCURRENT; i++)
+    {latch.offer("$i")}
+    //def job_list = ["test1","test2","test3","test4","test5","test6"]
+    def job_list=helper.getSubFolders(projectFolder)
+
+    for(int i=0; i<job_list.size(); i++) {
+        def name = job_list[i]
+        branches[name] = {
+            def thing = null
+            waitUntil {
+                thing = latch.pollFirst();
+                return thing != null;
+            }
+            try {
+                //execute job
+                //build(job: name, propagate: false)
+                helper.echoName(name)
+                //TODO backup environment and database
+                installersJson(projectFolder,name)
+                def propsSet=readProperty.get(projectFolder,name)
+                officeConnector(propsSet)
+                //TODO clean downloadPath
+            }finally {
+                //release a resource
+                latch.offer(thing)
+            }
+        }
+    }
+    parallel branches
+}
+
+def inOneJob(String projectFolder, String officeHook){
+    def branches=[:]
+    def MAX_CONCURRENT = 2
+    //create a fifo
+    latch = new java.util.concurrent.LinkedBlockingDeque(MAX_CONCURRENT)
+    //put resource in fifo
+    for(int i=0; i<MAX_CONCURRENT; i++)
+    {latch.offer("$i")}
+    //def job_list = ["test1","test2","test3","test4","test5","test6"]
+    def job_list=helper.getSubFolders(projectFolder)
+
+    for(int i=0; i<job_list.size(); i++) {
+        def name = job_list[i]
+        branches[name] = {
+            def thing = null
+            waitUntil {
+                thing = latch.pollFirst();
+                return thing != null;
+            }
+            try {
+                //execute job
+                //build(job: name, propagate: false)
+                helper.echoName(name)
+                //TODO backup environment and database
+                installersJson(projectFolder,name)
+                def propsSet=readProperty.get(projectFolder,name)
+                officeConnector.connectNewHook(propsSet,officeHook)
+                //TODO clean downloadPath
+            }finally {
+                //release a resource
+                latch.offer(thing)
+            }
+        }
+    }
+    parallel branches
+}
+
+

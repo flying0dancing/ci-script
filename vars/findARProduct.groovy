5cc254008b59f1448668f5c6b209319ddd61c445
Kun Shen <Kun.Shen@lombardrisk.com>
2020-03-23 11:10:10 +0800

    update product description and test get properties

--- vars/findARProduct.groovy
+++ vars/findARProduct.groovy
@@ -7,18 +7,17 @@ def call(installer,projectName,propertiesSet){
     def downloadFileFullName
     def downloadFileName
     echo '=============================== install '+iPrefix+' =================================='
-    downloadFileFullName=searchInstaller.searchLatestProduct(propertiesSet,iPrefix.toUpperCase(),mainVersion,buildNumber)
-    downloadFileName=helper.getFileName(downloadFileFullName)
-    echo 'buildNumber:'+buildNumber
-    echo 'downloadFileFullName:'+downloadFileFullName
-    echo 'downloadFileName:'+downloadFileName
-
     createHtmlContent('headline',' * ['+iPrefix+', '+iVersion+']')
     createHtmlContent('stepStartFlag')
     if(needInstall && needInstall.equalsIgnoreCase("no")){
         echo "no need to install product ["+iPrefix+", "+iVersion+" ]"
         createHtmlContent('stepline','install product: no need, skip')
     }else{
+        downloadFileFullName=searchInstaller.searchLatestProduct(propertiesSet,iPrefix.toUpperCase(),mainVersion,buildNumber)
+        downloadFileName=helper.getFileName(downloadFileFullName)
+
+        echo 'downloadFileFullName:'+downloadFileFullName
+        echo 'downloadFileName:'+downloadFileName
         if(downloadFileName){
             def flag=searchInstaller.checkNeedInstallOrNot(propertiesSet,downloadFileName)
             if(flag==0){
@@ -42,32 +41,41 @@ def call(installer,projectName,propertiesSet){
             createHtmlContent('stepline','install product: cannot find, skip')
         }
     }
-    handleConfigProps(installer.props,projectName,propertiesSet,iPrefix,downloadFileName)
+    if(installer.props){
+        if(!downloadFileName){
+            downloadFileFullName=searchInstaller.searchLatestProduct(propertiesSet,iPrefix.toUpperCase(),mainVersion,buildNumber)
+            downloadFileName=helper.getFileName(downloadFileFullName)
+        }
+        if(downloadFileName){
+            handleConfigProps(installer.props,projectName,propertiesSet,iPrefix,downloadFileName)
+        }
+    }else{
+        createHtmlContent('stepline','config product: no need, skip')
+    }
+
     createHtmlContent('stepEndFlag')
 }
 
 def handleConfigProps(props,projectName,propertiesSet,iPrefix,downloadFileName){
-    if(props && downloadFileName){
-        def installVersion=helper.getInstallerRealVersion(downloadFileName)
-        for(int j=0;j<props.size();j++){
-            def propy=props[j]
-            def needConfig=propy.needConfig
-            echo "========= DID configure $propy.filename ========="
-            if(needConfig && needConfig.equalsIgnoreCase("no")){
-                echo "no need to config ${propy}"
-                createHtmlContent('stepline','config '+propy+': no need, skip')
+    def installVersion=helper.getInstallerRealVersion(downloadFileName)
+    for(int j=0;j<props.size();j++){
+        def propy=props[j]
+        def needConfig=propy.needConfig
+        echo "========= DID configure $propy.filename ========="
+        if(needConfig && needConfig.equalsIgnoreCase("no")){
+            echo "no need to config ${propy}"
+            createHtmlContent('stepline','config '+propy+': no need, skip')
+        }else{
+            def metaData=propy.REPORTERMetadata
+            def eaFlag='1' //1 means config argument is -ea
+            if(metaData && metaData.equalsIgnoreCase("yes")){
+                echo "config REPORTER metadata ${propy}"
+                eaFlag='2' //2 means config argument is -da and -aa
             }else{
-                def metaData=propy.REPORTERMetadata
-                def eaFlag='1' //1 means config argument is -ea
-                if(metaData && metaData.equalsIgnoreCase("yes")){
-                    echo "config REPORTER metadata ${propy}"
-                    eaFlag='2' //2 means config argument is -da and -aa
-                }else{
-                    echo "config PRODUCT ${propy}"
-                }
-                createHtmlContent('steplineP1','config '+propy)
-                linkARProductDID(projectName,propertiesSet,iPrefix,installVersion,propy,eaFlag)
+                echo "config PRODUCT ${propy}"
             }
+            createHtmlContent('steplineP1','config '+propy)
+            linkARProductDID(projectName,propertiesSet,iPrefix,installVersion,propy,eaFlag)
         }
     }
 }

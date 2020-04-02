cb60c4db8ac5b296e37d60252256fc1e84382374
Kun Shen <Kun.Shen@lombardrisk.com>
2020-04-02 15:39:13 +0800

    try to copy trump to c driver

---
+++ vars/autCheckout.groovy
@@ -0,0 +1,29 @@
+
+def call(targetDir,repoName='trump',branch='branch_v1.2.1'){
+    def proj='cprod'
+    if(repoName.equalsIgnoreCase('trump') || repoName.equalsIgnoreCase('public')){
+        proj='aut'
+    }
+    checkout changelog: false, poll: false, scm: [
+            $class: 'GitSCM',
+            branches: [[name: "*/${branch}"]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [
+                    [$class: 'RelativeTargetDirectory', relativeTargetDir: "${targetDir}"],
+                    [$class: 'IgnoreNotifyCommit'],
+                    [$class: 'CleanBeforeCheckout'],
+                    [$class: 'CloneOption', depth: 0, honorRefspec: true, noTags: true, reference: '', shallow: true]
+            ],
+            submoduleCfg: [],
+            userRemoteConfigs: [
+                    [credentialsId: '4775e132-d845-4896-971d-f2a210ccdb02',
+                     url: "ssh://git@bitbucket.lombardrisk.com:7999/${proj}/${repoName}.git"]
+            ]
+    ]
+}
+
+/*
+trumpCheckout('trump','trump','branch_v1.2.1')
+trumpCheckout('trump/public','public','arproduct')
+trumpCheckout('ci-script','ci-script','branch_v1.2.1')
+*/

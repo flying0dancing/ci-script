28b4516de92a2f7050dba5085f13045f4f1a22c2
Kun Shen <Kun.Shen@lombardrisk.com>
2020-04-09 14:54:57 +0800

    draft

--- vars/copyTrump.groovy
+++ vars/copyTrump.groovy
@@ -8,8 +8,8 @@ def call(workspace, projectFolder, deployFolder, destParentDir="c:\\ar_auto"){
     def dest="$destParentDir\\trump-sel"
     def files="*.*"
     echo "copy trump-sel to $destParentDir"
-    def flag=bat(returnStatus: true, script: "robocopy $src $dest $files /E /NP 1>nul").trim()
-    if(flag==0){
+    def flag=bat(returnStatus: true, script: "robocopy $src $dest $files /E /NP 1>nul")
+    if("$flag"=="0"){
         //src="$workspace\\trump\\public"
         //dest="$destParentDir"
         //files="*.*"
@@ -25,7 +25,7 @@ def call(workspace, projectFolder, deployFolder, destParentDir="c:\\ar_auto"){
         src="$workspace\\scripts"
         dest="$destParentDir\\trump-sel"
         files="RunTest.bat RunTest1.bat"
-        echo "update test.json and test.properties in trump-sel"
+        echo "copy RunTest.bat in trump-sel"
         flag=bat(returnStatus: true, script: "robocopy $src $dest $files /NP")
         //dest="%USERPROFILE%"
         //files="getNewFullName.bat"

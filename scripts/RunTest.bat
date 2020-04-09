516ca4672c070cf68df1846cf0804395cc9ef6b2
Kun Shen <Kun.Shen@lombardrisk.com>
2020-04-09 16:35:01 +0800

    draft

--- scripts/RunTest.bat
+++ scripts/RunTest.bat
@@ -5,8 +5,8 @@ set resultfolder=%~3
 
 for /f "tokens=2 delims=:" %%a in ('ipconfig ^|find "IPv4 Address"') do for /f "tokens=4 delims=." %%b in ("%%a") do (set "machine=%%b")
 set regulator=%~4
-
-curl -XDELETE http://172.20.30.89:9200/log%machine%-%regulator%-*
+@echo %regulator%
+rem curl -XDELETE http://172.20.30.89:9200/log%machine%-%regulator%-*
 
 @echo Begin automation testing
 @echo mvn test -DxmlFileName=%filename% -DsrcFolder=%srcfolder% -DresultFolder=%resultfolder% -Dgo
